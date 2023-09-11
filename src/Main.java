54c796d2ef9568c3d751e7cf485972b086a9df67
mn0912 <monique.ngan@mail.utoronto.ca>
2023-09-11 12:44:09 -0400

    Called FizzBuzz functions

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main(); //doesn't work, but maybe if you remove string arg?
+    }
+}
